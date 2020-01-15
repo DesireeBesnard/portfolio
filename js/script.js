57a6561a3323c33d8a53a52e68d24e55fdd8b9bc
DesireeBesnard <desireebesnard@gmail.com>
2020-01-15 14:59:59 +0000

    fin de structuration du header avec integration background video

--- js/script.js
+++ js/script.js
@@ -12,10 +12,16 @@ $(function() {
 
 // end scroll button
 
+// END BOTTOM HEADER
 
-// end bottom header
 
-// start competences
+//START COMPETENCES
+
+
+//END COMPETENCES
+
+
+// START PROJETS
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -359,4 +365,4 @@ sliderEl.addEventListener('touchstart', stopAutoSlide);
 
 timer = setTimeout(autoSlide, 2000);
 
-// end competences
+// END PROJETS
